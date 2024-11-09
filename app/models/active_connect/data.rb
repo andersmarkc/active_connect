2a3095f5edf6350045bd31cb512cf8b8bb2d2359
andersmarkc <andersmarkc@gmail.com>
2024-11-09 21:37:22 +0100

    Start project

--- app/models/active_connect/data.rb
+++ app/models/active_connect/data.rb
@@ -2,7 +2,7 @@
 # Path: app/models/active_scrape/scrape_data.rb
 
 module ActiveConnect
-  class ActiveConnectData < ApplicationRecord
+  class Data < ApplicationRecord
     # Your model code here
   end
 end
--- test/models/active_connect_data_test.rb
+++ test/models/active_connect_data_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class ActiveConnectDataTest < ActiveSupport::TestCase
   test "can create ActiveConnectData" do
-    data = ActiveConnect::ActiveConnectData.create(run_at: Time.now, status: 1)
+    data = ActiveConnect::Data.create(run_at: Time.now, status: 1)
     assert data.persisted?, "Expected data to be saved in the database"
   end
 end
